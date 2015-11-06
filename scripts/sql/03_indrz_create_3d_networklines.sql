0cdce62f33128802440fea719752a45679ba0542
Michael Diener <mdiener21@gmail.com>
2015-11-06 23:32:55 +0100

    added routing models

--- scripts/sql/03_indrz_create_3d_networklines.sql
+++ scripts/sql/03_indrz_create_3d_networklines.sql
@@ -1,53 +1,89 @@
 -- if not, go ahead and update
 -- make sure tables dont exist
 
-drop table if exists geodata.ch08_e01_networklines_routing;
-drop table if exists geodata.ch08_e02_networklines_routing;
+drop table if exists geodata.networklines_ug01;
+drop table if exists geodata.networklines_e00;
+drop table if exists geodata.networklines_e01;
+drop table if exists geodata.networklines_e02;
+drop table if exists geodata.networklines_e03;
+
 
 -- convert to 3d coordinates with EPSG:3857
-SELECT ogc_fid, ST_Force_3d(ST_Transform(ST_Force_2D(st_geometryN(wkb_geometry, 1)),3857)) AS wkb_geometry,
-  type_id, cost, length, 0 AS source, 0 AS target
-  INTO geodata.ch08_e01_networklines_routing
-  FROM geodata.ch08_e01_networklines;
+SELECT id, ST_Force_3d(ST_Transform(ST_Force_2D(st_geometryN(geom, 1)),3857)) AS geom,
+  network_type, cost, length, 0 AS source, 0 AS target
+  INTO geodata.networklines_ug01
+  FROM django.routing_networklinesug01;
 
-SELECT ogc_fid, ST_Force_3d(ST_Transform(ST_Force_2D(st_geometryN(wkb_geometry, 1)),3857)) AS wkb_geometry,
-  type_id, cost, length, 0 AS source, 0 AS target
-  INTO geodata.ch08_e02_networklines_routing
-  FROM geodata.ch08_e02_networklines;
+SELECT id, ST_Force_3d(ST_Transform(ST_Force_2D(st_geometryN(geom, 1)),3857)) AS geom,
+  network_type, cost, length, 0 AS source, 0 AS target
+  INTO geodata.networklines_e00
+  FROM django.routing_networklinese00;
+
+SELECT id, ST_Force_3d(ST_Transform(ST_Force_2D(st_geometryN(geom, 1)),3857)) AS geom,
+  network_type, cost, length, 0 AS source, 0 AS target
+  INTO geodata.networklines_e01
+  FROM django.routing_networklinese01;
+
+SELECT id, ST_Force_3d(ST_Transform(ST_Force_2D(st_geometryN(geom, 1)),3857)) AS geom,
+  network_type, cost, length, 0 AS source, 0 AS target
+  INTO geodata.networklines_e02
+  FROM django.routing_networklinese02;
+
+SELECT id, ST_Force_3d(ST_Transform(ST_Force_2D(st_geometryN(geom, 1)),3857)) AS geom,
+  network_type, cost, length, 0 AS source, 0 AS target
+  INTO geodata.networklines_e03
+  FROM django.routing_networklinese03;
 
 -- fill the 3rd coordinate according to their floor number
-UPDATE geodata.ch08_e01_networklines_routing SET wkb_geometry=ST_Translate(ST_Force_3Dz(wkb_geometry),0,0,1);
-UPDATE geodata.ch08_e02_networklines_routing SET wkb_geometry=ST_Translate(ST_Force_3Dz(wkb_geometry),0,0,2);
+UPDATE geodata.networklines_ug01 SET geom=ST_Translate(ST_Force_3Dz(geom),0,0,-1);
+UPDATE geodata.networklines_e00 SET geom=ST_Translate(ST_Force_3Dz(geom),0,0,0);
+UPDATE geodata.networklines_e01 SET geom=ST_Translate(ST_Force_3Dz(geom),0,0,1);
+UPDATE geodata.networklines_e02 SET geom=ST_Translate(ST_Force_3Dz(geom),0,0,2);
+UPDATE geodata.networklines_e03 SET geom=ST_Translate(ST_Force_3Dz(geom),0,0,3);
 
-
-UPDATE geodata.ch08_e01_networklines_routing SET length =ST_Length(wkb_geometry);
-UPDATE geodata.ch08_e02_networklines_routing SET length =ST_Length(wkb_geometry);
+UPDATE geodata.networklines_ug01 SET length =ST_Length(geom);
+UPDATE geodata.networklines_e00 SET length =ST_Length(geom);
+UPDATE geodata.networklines_e01 SET length =ST_Length(geom);
+UPDATE geodata.networklines_e02 SET length =ST_Length(geom);
+UPDATE geodata.networklines_e03 SET length =ST_Length(geom);
 
 -- no cost should be 0 or NULL/empty
-UPDATE geodata.ch08_e01_networklines_routing SET cost=1 WHERE cost=0 or cost IS NULL;
-UPDATE geodata.ch08_e02_networklines_routing SET cost=1 WHERE cost=0 or cost IS NULL;
+UPDATE geodata.networklines_ug01 SET cost=1 WHERE cost=0 or cost IS NULL;
+UPDATE geodata.networklines_e00 SET cost=1 WHERE cost=0 or cost IS NULL;
+UPDATE geodata.networklines_e01 SET cost=1 WHERE cost=0 or cost IS NULL;
+UPDATE geodata.networklines_e02 SET cost=1 WHERE cost=0 or cost IS NULL;
+UPDATE geodata.networklines_e03 SET cost=1 WHERE cost=0 or cost IS NULL;
 
-
--- update unique ids ogc_fid accordingly
-UPDATE geodata.ch08_e01_networklines_routing SET ogc_fid=ogc_fid+100000;
-UPDATE geodata.ch08_e02_networklines_routing SET ogc_fid=ogc_fid+200000;
-
+-- update unique ids id accordingly
+UPDATE geodata.networklines_ug01 SET id=id+100000;
+UPDATE geodata.networklines_e00 SET id=id+200000;
+UPDATE geodata.networklines_e01 SET id=id+300000;
+UPDATE geodata.networklines_e02 SET id=id+400000;
+UPDATE geodata.networklines_e03 SET id=id+500000;
 
 -- merge all networkline floors into a single table for routing
 DROP TABLE IF EXISTS geodata.networklines_3857;
 SELECT * INTO geodata.networklines_3857 FROM
 (
-(SELECT ogc_fid, wkb_geometry, length, type_id, length*o1.cost as total_cost,
-   1 as layer FROM geodata.ch08_e01_networklines_routing o1) UNION
-(SELECT ogc_fid, wkb_geometry, length, type_id, length*o2.cost as total_cost,
-   2 as layer FROM geodata.ch08_e02_networklines_routing o2))
-as foo ORDER BY ogc_fid;
+  (SELECT id, geom, length, network_type, length*ug01.cost as total_cost,
+   1 as layer FROM geodata.networklines_ug01 ug01) UNION
+  (SELECT id, geom, length, network_type, length*e0.cost as total_cost,
+   1 as layer FROM geodata.networklines_e00 e0) UNION
+(SELECT id, geom, length, network_type, length*e1.cost as total_cost,
+   1 as layer FROM geodata.networklines_e01 e1) UNION
+(SELECT id, geom, length, network_type, length*e2.cost as total_cost,
+   2 as layer FROM geodata.networklines_e02 e2) UNION
+  (SELECT id, geom, length, network_type, length*e3.cost as total_cost,
+   2 as layer FROM geodata.networklines_e03 e3)
 
-CREATE INDEX wkb_geometry_gist_index
-   ON geodata.networklines_3857 USING gist (wkb_geometry);
+)
+as foo ORDER BY id;
 
-CREATE INDEX ogc_fid_idx
-   ON geodata.networklines_3857 USING btree (ogc_fid ASC NULLS LAST);
+CREATE INDEX geom_gist_index
+   ON geodata.networklines_3857 USING gist (geom);
+
+CREATE INDEX id_idx
+   ON geodata.networklines_3857 USING btree (id ASC NULLS LAST);
 
 CREATE INDEX network_layer_idx
   ON geodata.networklines_3857
@@ -58,25 +94,28 @@ CREATE INDEX network_layer_idx
 SELECT Populate_Geometry_Columns('geodata.networklines_3857'::regclass);
 
 -- update stairs, ramps and elevators to match with the next layer
-UPDATE geodata.networklines_3857 SET wkb_geometry=ST_AddPoint(wkb_geometry,
-  ST_EndPoint(ST_Translate(wkb_geometry,0,0,1)))
-  WHERE type_id=3 OR type_id=5 OR type_id=7;
+UPDATE geodata.networklines_3857 SET geom=ST_AddPoint(geom,
+  ST_EndPoint(ST_Translate(geom,0,0,1)))
+  WHERE network_type=1 OR network_type=2 OR network_type=7;
 -- remove the second last point
-UPDATE geodata.networklines_3857 SET wkb_geometry=ST_RemovePoint(wkb_geometry,ST_NPoints(wkb_geometry) - 2)
-  WHERE type_id=3 OR type_id=5 OR type_id=7;
+UPDATE geodata.networklines_3857 SET geom=ST_RemovePoint(geom,ST_NPoints(geom) - 2)
+  WHERE network_type=1 OR network_type=2 OR network_type=7;
 
 
 -- add columns source and target
 ALTER TABLE geodata.networklines_3857 add column source integer;
 ALTER TABLE geodata.networklines_3857 add column target integer;
-ALTER TABLE geodata.networklines_3857 OWNER TO postgres;
+ALTER TABLE geodata.networklines_3857 OWNER TO "indrz-pg";
 
 -- we dont need the temporary tables any more, delete them
-DROP TABLE IF EXISTS geodata.ch08_e01_networklines_routing;
-DROP TABLE IF EXISTS geodata.ch08_e02_networklines_routing;
+DROP TABLE IF EXISTS geodata.networklines_ug01;
+DROP TABLE IF EXISTS geodata.networklines_e00;
+DROP TABLE IF EXISTS geodata.networklines_e01;
+DROP TABLE IF EXISTS geodata.networklines_e02;
+DROP TABLE IF EXISTS geodata.networklines_e03;
 
 -- remove route nodes vertices table if exists
 DROP TABLE IF EXISTS geodata.networklines_3857_vertices_pgr;
 -- building routing network vertices (fills source and target columns in those new tables)
-SELECT public.pgr_createTopology3d('geodata.networklines_3857', 0.0001, 'wkb_geometry', 'ogc_fid');
+SELECT public.pgr_createTopology3d('geodata.networklines_3857', 0.0001, 'geom', 'id');
 
---
+++ scripts/sql/indrz-routing-query.sql
@@ -0,0 +1,9 @@
+SELECT seq, id1 AS node, id2 AS edge, ST_Length(geom) AS cost,
+           ST_AsGeoJSON(geom) AS geoj
+      FROM pgr_dijkstra(
+        'SELECT id as id, source, target, st_length(geom) as cost
+         FROM geodata.networklines_3857',
+        8,21, FALSE, FALSE
+      ) AS dij_route
+      JOIN  geodata.networklines_3857 AS input_network
+      ON dij_route.id2 = input_network.id ;
