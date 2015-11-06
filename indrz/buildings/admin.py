2bf64bca54da8cd04f2a59066808e62e41f5e852
Michael Diener <mdiener21@gmail.com>
2015-11-06 23:42:33 +0100

    removed routing networklines from admin

--- indrz/buildings/admin.py
+++ indrz/buildings/admin.py
@@ -4,7 +4,7 @@ from django.contrib.gis import admin as gis_admin
 from buildings.models import LtAccessType, LtSpaceType, LtCondition
 from buildings.models import  Organization, Campus, Building
 from buildings.models import  BuildingFloor, BuildingFloorSpace, InteriorFloorSection
-from routing.models import Networklines
+# from routing.models import NetworklinesE00
 
 
 admin.site.register(Organization, gis_admin.OSMGeoAdmin)
@@ -13,7 +13,8 @@ admin.site.register(Building, gis_admin.OSMGeoAdmin)
 admin.site.register(BuildingFloor, gis_admin.OSMGeoAdmin)
 admin.site.register(BuildingFloorSpace, gis_admin.OSMGeoAdmin)
 admin.site.register(InteriorFloorSection, gis_admin.OSMGeoAdmin)
-admin.site.register(Networklines, gis_admin.OSMGeoAdmin)
+
+# admin.site.register(NetworklinesE00, gis_admin.OSMGeoAdmin)
 
 admin.site.register(LtAccessType)
 admin.site.register(LtSpaceType)
