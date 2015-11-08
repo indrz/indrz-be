41a41798f942e24b793883410db26f668c74a99a
Michael Diener <mdiener21@gmail.com>
2015-11-08 22:08:41 +0100

    added conference model

--- indrz/conference/models.py
+++ indrz/conference/models.py
@@ -1,14 +1,27 @@
-from buildings.models import BuildingFloor
+from buildings.models import BuildingFloor, Building
 from django.contrib.gis.db import models as gis_models
 from django.utils.translation import ugettext_lazy as _
 from buildings.models import OrganizationInfoBase
+
+
+class Conference(gis_models):
+    """
+    Conference information
+    """
+
+    name = gis_models.CharField(verbose_name=_(u"Conference name"), max_length=150, null=True, blank=True)
+    year = gis_models.IntegerField(verbose_name=_(u"Year of conference"), null=True, blank=True)
+    long_name = gis_models.CharField(verbose_name='long name', null=True, max_length=255, blank=True)
+    fk_building = gis_models.ForeignKey(Building, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
 
 
 class Exhibitor(OrganizationInfoBase):
     """
     Exhibitor information
     """
-
 
     pass
 
@@ -18,18 +31,16 @@ class Stands(gis_models.Model):
     Conference Stand locations
     """
 
-    short_name = gis_models.CharField(verbose_name=_(u"short name of stand"), max_length=150, null=True, blank=True)
-    long_name = gis_models.CharField(verbose_name=_(u"long stand name"), max_length=150, null=True, blank=True)
-    label = gis_models.CharField(verbose_name=_(u"label of stand"), max_length=150, null=True, blank=True)
-    foyer_location = gis_models.CharField(verbose_name=_(u"foyer name location value"), max_length=150, null=True, blank=True)
-    stand_id_external = gis_models.CharField(verbose_name=_(u"stand id from external system"), max_length=150, null=True, blank=True)
-    stand_num_external = gis_models.CharField(verbose_name=_(u"stand number from external"), max_length=150, null=True, blank=True)
-    stand_type = gis_models.CharField(verbose_name=_(u"type of stand"), max_length=150, null=True, blank=True)
-
+    short_name = gis_models.CharField(verbose_name=_("short name of stand"), max_length=150, null=True, blank=True)
+    long_name = gis_models.CharField(verbose_name=_("long stand name"), max_length=150, null=True, blank=True)
+    label = gis_models.CharField(verbose_name=_("label of stand"), max_length=150, null=True, blank=True)
+    foyer_location = gis_models.CharField(verbose_name=_("foyer name location value"), max_length=150, null=True, blank=True)
+    stand_id_external = gis_models.CharField(verbose_name=_("stand id from external system"), max_length=150, null=True, blank=True)
+    stand_num_external = gis_models.CharField(verbose_name=_("stand number from external"), max_length=150, null=True, blank=True)
+    stand_type = gis_models.CharField(verbose_name=_("type of stand"), max_length=150, null=True, blank=True)
 
     fk_building_floor = gis_models.ForeignKey(BuildingFloor)
     fk_exhibitor = gis_models.ForeignKey(Exhibitor)
-
 
     multi_polygon = gis_models.MultiPolygonField(srid=3857, spatial_index=True, db_column='geom', null=True, blank=True)
     objects = gis_models.GeoManager()
@@ -38,7 +49,4 @@ class Stands(gis_models.Model):
         ordering = ['fk_building_floor']
 
     def __str__(self):
-        return str(self.short_name) or u''
-
-
-
+        return str(self.short_name) or ''
