844af69bfdd5cbe92045db9fafb3f3bdf612fc52
Michael Diener <mdiener21@gmail.com>
2015-11-10 15:37:18 +0100

    updated url calls

--- indrz/api/urls.py
+++ indrz/api/urls.py
@@ -4,10 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = patterns('api.views',
     #  ex valid call from to  /api/directions/1587848.414,5879564.080,2&1588005.547,5879736.039,2
     #url(r'^directions/(?P<start_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<start_floor>\d+)&(?P<end_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<end_floor>\d+)&(?P<route_type>[0-9])/$', 'create_route', name='directions'),
-    url(r'^directions/(?P<start_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<start_floor>[-]?\d+)&(?P<end_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<end_floor>[-]?\d+)&(?P<route_type>[0-9])/$', 'create_route', name='directions2'),
-    url(r'^directions/(?P<start_room_num>\d{5})&(?P<end_room_num>\d{5})&(?P<route_type>[0-9])/$', 'route_room_to_room', name='route-room-to-room'),
+    url(r'^directions/(?P<start_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<start_floor>[-]?\d+)&(?P<end_coord>[-]?\d+\.?\d+,\d+\.\d+),(?P<end_floor>[-]?\d+)&(?P<route_type>[0-9])/$', 'create_route_from_coords', name='root_coords'),
+    url(r'^directions/(?P<start_room_key>\d{5})&(?P<end_room_key>\d{5})&(?P<route_type>[0-9])/$', 'route_room_to_room', name='route-room-to-room'),
     url(r'^directions/(?P<room_num>\d{5})/$', 'get_room_centroid_node', name='room-center'),
-    url(r'^rooms/$', 'room_list', name='room-list'),
+    url(r'^spaces/search/$', 'spaces_list', name='spaces_list'),
     url(r'^directions/(?P<building_name>building=d{1,5})&start={1,6}&destination={1,6}/$', 'route_room_to_room', name='route-space-to-space' )
 
 )
