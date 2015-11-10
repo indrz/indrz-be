14a3b3c40c47cea356e2d327a897171a56cd282e
mdiener21 <mdiener21@gmail.com>
2015-11-10 21:08:29 +0100

    added init sql start proj

---
+++ scripts/sql/00_setup_indrz_db.sql
@@ -0,0 +1,26 @@
+CREATE ROLE "indrz-pg" LOGIN ENCRYPTED PASSWORD 'md539091991722381dcef6112d1b681b5d5'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+
+ALTER ROLE "indrz-pg"
+  SET search_path = django, geodata, public;
+
+
+CREATE DATABASE indrz
+  WITH OWNER = "indrz-pg"
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'German_Austria.1252'
+       LC_CTYPE = 'German_Austria.1252'
+       CONNECTION LIMIT = -1;
+
+CREATE EXTENSION pgrouting
+  SCHEMA public
+  VERSION "2.0.0";
+
+CREATE EXTENSION postgis
+  SCHEMA public
+  VERSION "2.1.7";
+
+CREATE SCHEMA django AUTHORIZATION "indrz-pg";
+CREATE SCHEMA geodata AUTHORIZATION "indrz-pg";
+
