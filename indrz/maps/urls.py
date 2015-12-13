098129521a2e0e65956952736071d67fb525ddbc
Michael Diener <mdiener21@gmail.com>
2015-12-13 19:52:44 +0100

    added new base-map template

--- indrz/maps/urls.py
+++ indrz/maps/urls.py
@@ -4,7 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 # all urls begin with http://localhost:8000/maps/
 urlpatterns = patterns('maps.views',
     #  ex valid call from to  /api/directions/1587848.414,5879564.080,2&1588005.547,5879736.039,2
-    url(r'^(?P<map_name>[0-9a-zA-Z_-]+)/$', 'route_map', name='map_name'),
+    # url(r'^(?P<map_name>[0-9a-zA-Z_-]+)/$', 'route_map', name='map_name'),
+    url(r'^$', 'map_socgen_nantes', name='socgen-nantes'),
+    url(r'^socgen-nantes/$', 'map_socgen_nantes', name='socgen-nantes'),
+    url(r'^testswitch/$', 'testswitch', name='testswitch'),
     url(r'^space/(?P<space_id>\d{1,5})(?P<zoom_level>&zoom=\d{1,2})?/$', 'zoom_space', name='zoom-space'),
     url(r'^campus/(?P<campus_id>\d{1,5})(?P<zoom_level>&zoom=\d{1,2})?/$', 'zoom_campus', name='zoom-campus'),
     url(r'^building/(?P<building_id>\d{1,5})(?P<zoom_level>&zoom=\d{1,2})?/$', 'zoom_building', name='zoom-building'),
--- indrz/maps/views.py
+++ indrz/maps/views.py
@@ -2,33 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def route_map(request, map_name):
-    return render(request, str(map_name)+ '.html')
+def map_socgen_nantes(request):
+    return render(request, 'socgen-nantes.html')
 
 
-def zoom_campus(request, campus_id, zoom_level=17):
-    """
-    Zoom to a specific indoor space such as an office
-    :return:
-    """
-    pass
+def testswitch(request):
+    return render(request, 'testswitch.html')
 
 
-def zoom_building(request, building_id, zoom_level=18):
-    """
-    Zoom to a specific building
-    :return:
-    """
-    pass
-
-
-def zoom_space(request, space_id, zoom_level=20):
-    """
-    Zoom to a specific indoor space such as an office
-    :return:
-    """
-    pass
-
-
-def index(request):
-    return render(request, 'route-map.html')
+# def index(request):
+#     return render(request, 'route-map.html')
