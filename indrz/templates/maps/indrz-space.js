c8423a50d60a304ea4ec4da2a3b4a5677a58308c
Michael Diener <mdiener21@gmail.com>
2015-12-17 12:06:56 +0100

    fixed styling selected space with text label

--- indrz/templates/maps/indrz-space.js
+++ indrz/templates/maps/indrz-space.js
@@ -1,3 +1,39 @@
+    var style = new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(255, 255, 255, 0.6)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: '#319FD3',
+        width: 1
+      }),
+      text: new ol.style.Text({
+        font: 'bold 12px Arial,sans-serif',
+        fill: new ol.style.Fill({
+          color: '#000'
+        }),
+          maxResolution : 2000,
+        stroke: new ol.style.Stroke({
+          color: '#fff',
+          width: 3
+        })
+      })
+    });
+
+var styles = [style];
+      var getText = function(feature, resolution) {
+
+                      if (feature ) {
+
+                          if (feature.get('short_name') !== null)
+                          {
+                              //info.innerHTML = feature.getId() + ': ' + feature.get('name');
+                              style.getText().setText(resolution < 0.1 ? feature.get('short_name') : '');
+                                return styles;
+
+                              }
+                          }
+                    };
+
 var spaceJSONURL = 'http://localhost:8000/api/v1/buildings/spaces/'+ space_id +'.json';
 
 var space_source = new ol.source.Vector();
@@ -7,29 +43,10 @@ $.ajax(spaceJSONURL).then(function(response) {
         {featureProjection: 'EPSG:4326'});
     space_source.addFeatures(features);
 });
-var style = new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(255, 255, 255, 0.6)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#319FD3',
-      width: 1
-    }),
-    image: new ol.style.Circle({
-      radius: 5,
-      fill: new ol.style.Fill({
-        color: 'rgba(255, 255, 255, 0.6)'
-      }),
-      stroke: new ol.style.Stroke({
-        color: '#319FD3',
-        width: 1
-      })
-    })
-});
 
 var spaceLayer = new ol.layer.Vector({
     source: space_source,
-    style:  style,
+    style: getText,
     title: "Space",
     name: "Space"
 });
