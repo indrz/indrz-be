3f0a462592b5f7e302b6732e24f2f4d77fc97fc7
konrad.rokos.up@gmail.com <daniel@daniel-desktop.(none)>
2015-12-18 11:45:09 +0100

    add popup closer

--- indrz/templates/maps/indrz-popup.js
+++ indrz/templates/maps/indrz-popup.js
@@ -1,22 +1,40 @@
-var popup = new ol.Overlay({
-    element: document.getElementById('indrz-popup')
-});
-map.addOverlay(popup);
+var popup_container = document.getElementById('indrz-popup');
+var popup_content = document.getElementById('popup-content');
+var popup_closer = document.getElementById('popup-closer');
 
-map.on('click', function (evt) {
-    var element = popup.getElement();
-    var coordinate = evt.coordinate;
-    var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
+/**
+ * Create an overlay to anchor the popup to the map.
+ */
+var popup_overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
+  element: popup_container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250
+  },
+  zIndex: 99
+}));
+
+/**
+ * Add a click handler to hide the popup.
+ * @return {boolean} Don't follow the href.
+ */
+popup_closer.onclick = function() {
+  popup_overlay.setPosition(undefined);
+  popup_closer.blur();
+  return false;
+};
+
+map.addOverlay(popup_overlay);
+
+
+/**
+ * Add a click handler to the map to render the popup.
+ */
+map.on('singleclick', function(evt) {
+  var coordinate = evt.coordinate;
+  var hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
         coordinate, 'EPSG:3857', 'EPSG:4326'));
 
-    $(element).popover('destroy');
-    popup.setPosition(coordinate);
-// the keys are quoted to prevent renaming in ADVANCED mode.
-    $(element).popover({
-        'placement': 'top',
-        'animation': false,
-        'html': true,
-        'content': '<p>Coordinate:</p><code>' + hdms + '</code><p><a href="#"><i class="fa fa-bug fa-fw"></i> Bug report</a>  </p> '
-    });
-    $(element).popover('show');
+  popup_content.innerHTML = '<p>Coordinate:</p><code>' + hdms + '</code><p><a href="#"><i class="fa fa-bug fa-fw"></i> Bug report</a>  </p>';
+  popup_overlay.setPosition(coordinate)
 });
