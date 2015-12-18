a209feb6ab7e982e8967d50bcc8774196da11281
konrad.rokos.up@gmail.com <daniel@daniel-desktop.(none)>
2015-12-18 10:46:15 +0100

    #18 add switcher button

--- indrz/templates/maps/indrz-controls.js
+++ indrz/templates/maps/indrz-controls.js
@@ -1,8 +1,18 @@
+var map_type = true;
+
 var full_screen_control = new ol.control.FullScreen({
     label: "Go Full Screen",
     className: "btn-fullscreen",
     target: document.getElementById("id-fullscreen")
 });
 
-
 map.addControl(full_screen_control);
+
+$("#id-map-switcher").on("click", function(evt){
+    map_type = !map_type;
+    if(map_type) {
+        $(this).text('Satellite');
+    } else {
+        $(this).text('Map');
+    }
+});
