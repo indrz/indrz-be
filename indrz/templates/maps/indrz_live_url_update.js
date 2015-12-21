5104f4e5f0a3b26f506a6e3cdf37dc6e0b0263f1
devstorm21 <konrad.rokos.up@gmail.com>
2015-12-21 10:33:29 +0100

    #16 done

---
+++ indrz/templates/maps/indrz_live_url_update.js
@@ -0,0 +1,19 @@
+map.on('moveend', function(e){
+    var current_extent = map.getView().calculateExtent(map.getSize());
+    var current_zoom = map.getView().getZoom();
+
+    var url = "/api/v1/left=" + current_extent[0] + "&right=" + current_extent[2] +
+        "&top=" + current_extent[3]+"&bottom=" + current_extent[1] + "&campusid=4&zlevel=" + current_zoom;
+
+    history.pushState(data, 'live_url_update', url);
+});
+
+window.addEventListener('popstate', function(event) {
+    updateContent(event.state);
+});
+
+function updateContent(data) {
+    if (data == null)
+        return;
+    console.log(data);
+}
