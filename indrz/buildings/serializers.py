5285a8b73ca0c0ffb6b5c7a25d7592560548a220
Michael Diener <mdiener21@gmail.com>
2015-12-21 11:18:24 +0100

    update api to master on branch 12

--- indrz/buildings/serializers.py
+++ indrz/buildings/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
-from buildings.models import Building, BuildingFloorSpace, LtSpaceType
+from buildings.models import Building, BuildingFloorSpace, LtSpaceType, BuildingFloor
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
-
 
 class BuildingSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,10 +9,14 @@ class BuildingSerializer(serializers.ModelSerializer):
 
 
 class BuildingFloorSpaceSerializer(GeoFeatureModelSerializer):
-    fk_building = BuildingSerializer(read_only=True)
-
     class Meta:
         model = BuildingFloorSpace
         geo_field = "multi_poly"
         fields = ('id', 'short_name', 'floor_num', 'multi_poly', 'fk_building', 'fk_building_floor',
                   'room_external_id', 'space_type')
+
+
+class BuildingFloorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BuildingFloor
+        fields = ('id', 'short_name', 'floor_num', 'fk_building')
