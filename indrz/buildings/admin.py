ea337113039d5436345d6b05b893be9ccbade4ad
Michael Diener <mdiener21@gmail.com>
2016-02-04 10:12:08 +0100

    #76 customized admin view

--- indrz/buildings/admin.py
+++ indrz/buildings/admin.py
@@ -4,22 +4,35 @@ from django.contrib.gis import admin as gis_admin
 from buildings.models import LtAccessType, LtSpaceType, LtCondition
 from buildings.models import  Organization, Campus, Building
 from buildings.models import  BuildingFloor, BuildingFloorSpace, InteriorFloorSection
-# from routing.models import NetworklinesE00
-
 
 admin.site.register(Organization, gis_admin.OSMGeoAdmin)
 admin.site.register(Campus, gis_admin.OSMGeoAdmin)
-admin.site.register(Building, gis_admin.OSMGeoAdmin)
-admin.site.register(BuildingFloor, gis_admin.OSMGeoAdmin)
-admin.site.register(BuildingFloorSpace, gis_admin.OSMGeoAdmin)
 admin.site.register(InteriorFloorSection, gis_admin.OSMGeoAdmin)
 
-# admin.site.register(NetworklinesE00, gis_admin.OSMGeoAdmin)
 class LtSpaceTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'code', 'name')
     search_fields = ('name',)
 
 
+class BuildingFloorSpaceAdmin(gis_admin.OSMGeoAdmin):
+    list_display = ('short_name', 'room_external_id', 'room_number', 'fk_building_floor', 'floor_num', 'space_type', 'id')
+    search_fields = ('short_name', 'fk_building')
+
+
+class BuildingFloorAdmin(gis_admin.OSMGeoAdmin):
+    list_display = ('short_name', 'fk_building', 'id')
+    search_fields = ('short_name',)
+
+
+class BuildingAdmin(gis_admin.OSMGeoAdmin):
+    list_display = ('building_name',  'fk_campus', 'fk_organization', 'id')
+    search_fields = ('building_name', 'fk_campus')
+
+
 admin.site.register(LtAccessType)
 admin.site.register(LtSpaceType, LtSpaceTypeAdmin)
 admin.site.register(LtCondition)
+
+admin.site.register(Building, BuildingAdmin )
+admin.site.register(BuildingFloor, BuildingFloorAdmin )
+admin.site.register(BuildingFloorSpace, BuildingFloorSpaceAdmin)
