36f0d880da71257141bd4d30c3a6f70d78bb0e90
Michael Diener <mdiener21@gmail.com>
2016-07-28 10:45:14 +0200

    fix order of SQL commands on db setup

--- scripts/sql/00_setup_indrz_db.sql
+++ scripts/sql/00_setup_indrz_db.sql
@@ -1,8 +1,6 @@
 CREATE ROLE "indrz-pg" LOGIN ENCRYPTED PASSWORD 'md539091991722381dcef6112d1b681b5d5'
   NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
 
-ALTER ROLE "indrz-pg" SET search_path = django, geodata, public;
-
 CREATE DATABASE indrz
   WITH OWNER = "indrz-pg"
        ENCODING = 'UTF8'
@@ -18,5 +16,7 @@ CREATE EXTENSION pgrouting SCHEMA public VERSION "2.1.0";
 
 CREATE SCHEMA django AUTHORIZATION "indrz-wu";
 CREATE SCHEMA geodata AUTHORIZATION "indrz-wu";
+
+ALTER ROLE "indrz-pg" SET search_path = django, geodata, public;
 
 
