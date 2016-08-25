de371a34cf0979be27cbbb605e73ad9df7dedd5b
Michael Diener <mdiener21@gmail.com>
2016-08-25 15:17:32 +0200

    added related names to buildings serializers output

--- indrz/buildings/serializers.py
+++ indrz/buildings/serializers.py
@@ -14,13 +14,15 @@ class CampusSerializer(serializers.ModelSerializer):
 
 
 class BuildingFloorSpaceSerializer(GeoFeatureModelSerializer):
+    building_name = serializers.StringRelatedField(source='fk_building', read_only=True)
+    floor_name = serializers.StringRelatedField(source='fk_building_floor', read_only=True)
+    type_name = serializers.StringRelatedField(source='space_type', read_only=True)
 
     class Meta:
         model = BuildingFloorSpace
         geo_field = "multi_poly"
         fields = ('id', 'short_name', 'floor_num', 'multi_poly', 'fk_building', 'fk_building_floor',
-                  'room_external_id', 'space_type')
-
+                  'room_external_id', 'space_type', 'building_name', 'floor_name', 'type_name')
 
 class BuildingFloorGeomSerializer(GeoFeatureModelSerializer):
 
@@ -31,11 +33,11 @@ class BuildingFloorGeomSerializer(GeoFeatureModelSerializer):
 
 
 class FloorSerializer(serializers.ModelSerializer):
-    # buildingfloorspace_set = BuildingFloorSpaceSerializer(many=True, read_only=True)
+    buildingfloorspace_set = BuildingFloorSpaceSerializer(many=True, read_only=True)
 
     class Meta:
         model = BuildingFloor
-        fields = ('id', 'short_name', 'floor_num', 'fk_building')
+        fields = ('id', 'short_name', 'floor_num', 'fk_building', 'buildingfloorspace_set')
 
 
 class BuildingSerializer(serializers.ModelSerializer):
