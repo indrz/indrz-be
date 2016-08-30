3b3a536554ac4807339c6a08688760b5ee91cf85
Michael Diener <mdiener21@gmail.com>
2016-08-30 17:06:08 +0200

    updated poi manager admin site

--- indrz/poi_manager/admin.py
+++ indrz/poi_manager/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 from poi_manager.models import PoiCategory, Poi, PoiIcon
 
-admin.site.register(Poi)
+class PoiAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'fk_poi_category', 'fk_building')
+    search_fields = ('name', 'fk_building',)
+    list_filter = ('fk_poi_category', 'fk_building')
+
+admin.site.register(Poi, PoiAdmin)
 admin.site.register(PoiIcon)
 admin.site.register(PoiCategory, MPTTModelAdmin)
