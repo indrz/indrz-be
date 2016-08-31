b2a71428f10e269e136b9d5ead85eff3f24690f0
Michael Diener <mdiener21@gmail.com>
2016-08-31 16:11:17 +0200

    added missing import

--- indrz/users/models.py
+++ indrz/users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import  ugettext_lazy as _
+
 from buildings.models import Organization, Campus
 
 
