38f069d161a137a41cd03fd6e622a174c980acd3
mdiener21 <mdiener21@gmail.com>
2019-09-29 08:24:34 +0200

    update homepage urls

--- indrz/homepage/urls.py
+++ indrz/homepage/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from homepage.views import view_map #view_help
-from api.search_tu import search_any, searchAutoComplete, TuApi
+from api.search_tu import TuApi
 # all urls begin with http://localhost:8000/map/
 
 urlpatterns = [
@@ -10,8 +10,7 @@ urlpatterns = [
     # url(r'^', view_map, name='map_name'),
     # /autocomplete/?query=Irene Fellner
     url(r'^$', view_map, name='map_home'),
-    url(r'^autocomplete/(?P<search_text>.+)', searchAutoComplete.as_view(), name='search_autocomplete'),
-    url(r'^search/(?P<q>.+)', search_any, name='search'),
+
     # # url(r'^map/(?P<poicatid>poi-cat-id=\d{1,4})(,\d{1,4})?(,\d{1,4})?(,\d{1,4})?(,\d{1,4})?', view_map, name='map_name'),
     # url(r'^map/(?P<map_name>[^/]+)/$', view_map, name='map_name'),
     # url(r'^help/$', view_help, name='help'),
