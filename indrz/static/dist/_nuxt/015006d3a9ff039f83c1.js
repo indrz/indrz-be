49f5c899950af9a7e0c1b6a40fde30bf1b7cad7c
mdiener21 <mdiener21@gmail.com>
2019-10-17 09:15:28 +0200

    added updated frontend

---
+++ indrz/static/dist/_nuxt/015006d3a9ff039f83c1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{534:function(n,l,o){"use strict";o.r(l);var e=o(498).default,t=o(39),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
