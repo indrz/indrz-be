545f3d4346b4b13d36006e72e50dc3fd10377c9c
mdiener21 <mdiener21@gmail.com>
2019-11-02 13:31:09 +0100

    deploy update NOT WORKING

--- devops/bin/deploy.sh
+++ devops/bin/deploy.sh
@@ -17,6 +17,37 @@ else
 fi
 
 
+#!/usr/bin/env bash
+CODE_BASE_PATH=/opt/git_repos/indrz-tu
+DEPLOY_FOLDER=/var/www/navigatur.tuwien.ac.at
+
+cd $CODE_BASE_PATH
+git pull
+
+
+cd $CODE_BASE_PATH/indrz
+source /opt/.venvs/indrz-tu/bin/activate
+python manage.py collectstatic --clear <<<yes
+
+
+sudo cp -r $DEPLOY_FOLDER/static/dist/. $DEPLOY_FOLDER/
+
+# clean up remove dist folder
+sudo rm -r $DEPLOY_FOLDER/static/dist
+
+sudo chown -R http-web:http-web /var/www/tu.indrz.com
+
+
+supervisorctl reload
+systemctl reload nginx
+
+sudo service supervisor reload
+sudo service nginx reload
+
+
+deactivate
+
+
 #docker build $NO_CACHE -t "$PROJECT_ID/$IMAGE_NAME" --build-arg ENV_TYPE=$ENV_TYPE -f "$DEVOPS_PATH/docker/$IMAGE_NAME/Dockerfile" $DEVOPS_PATH/docker/$IMAGE_NAME/
 
 docker build NO_CACHE="--no-cache" -t webapp:latest -f "devops/docker/webapp/Dockerfile"
