d0731a57939a53ba88c7048895cace1938dadb78
mdiener21 <mdiener21@gmail.com>
2019-11-24 13:37:47 +0100

    added new fields to poi serializer for api

--- indrz/poi_manager/serializers.py
+++ indrz/poi_manager/serializers.py
@@ -8,15 +8,18 @@ from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 class PoiSerializer(GeoFeatureModelSerializer):
     category_name = CharField(source='category.cat_name', read_only=True)
+    category_name_de = CharField(source='category.cat_name_de', read_only=True)
+    category_name_en = CharField(source='category.cat_name_en', read_only=True)
     category_icon = CharField(source='category.icon', read_only=True)
+    category_icon_css_name = CharField(source='category.icon_css_name', read_only=True)
 
     class Meta:
         model = Poi
         geo_field = 'geom'
         # fields = '__all__'
         # depth = 1
-        fields = ('id', 'name', 'name_en', 'name_de', 'floor_num', 'floor_name', 'icon', 'category', 'category_name',
-                  'category_icon')
+        fields = ('id', 'name', 'name_en', 'name_de', 'enabled', 'floor_num', 'floor_name', 'icon', 'category',
+                  'category_name', 'category_icon', 'category_name_en', 'category_name_de', 'category_icon_css_name')
 
 
 class PoiCategorySerializer(serializers.ModelSerializer):
