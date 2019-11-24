dddbf7e9ed5e298f0c0ae4108e24144446fd47ba
mdiener21 <mdiener21@gmail.com>
2019-11-24 13:03:26 +0100

    poi serializer updated to serve poi cat data

--- indrz/poi_manager/serializers.py
+++ indrz/poi_manager/serializers.py
@@ -1,22 +1,28 @@
 from rest_framework import serializers
+from rest_framework.fields import CharField
 
 from .models import Poi, PoiCategory
+
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 
 class PoiSerializer(GeoFeatureModelSerializer):
+    category_name = CharField(source='category.cat_name')
+    category_icon = CharField(source='category.icon')
+
     class Meta:
         model = Poi
         geo_field = 'geom'
-        fields = '__all__'
+        # fields = '__all__'
         # depth = 1
-        # fields = ('id', 'name', 'name_en', 'name_de', 'floor_num', 'floor_name', 'icon', )
+        fields = ('id', 'name', 'name_en', 'name_de', 'floor_num', 'floor_name', 'icon', 'category', 'category_name',
+                  'category_icon')
 
 
 class PoiCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = PoiCategory
-        fields = ('cat_name', 'cat_name_en', 'cat_name_de', 'icon', 'fk_poi_icon', 'enabled')
+        fields = ('id', 'cat_name', 'cat_name_en', 'cat_name_de', 'icon', 'fk_poi_icon', 'enabled')
 
 
 class PoiSimpleSerializer(serializers.ModelSerializer):
