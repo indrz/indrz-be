77ace45efd2bc8f4fe33f5f899ffdeeaee0ef976
mdiener21 <mdiener21@gmail.com>
2019-12-07 19:31:26 +0100

    update frontend tu static

---
+++ indrz/static/dist/_nuxt/64baf54b77bff7927f70.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{615:function(n,t,e){"use strict";e.r(t);var l={layout:"admin"},o=e(32),component=Object(o.a)(l,function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("Dashboard")])},[],!1,null,null,null);t.default=component.exports}}]);
