a1007f70ad1f4497290fbbb0a03ed73afdca4e71
mdiener21 <mdiener21@gmail.com>
2021-02-07 15:02:38 +0100

    update paths to more generic names

--- devops/scripts/deploy-indrz.sh
+++ devops/scripts/deploy-indrz.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-CODE_BASE_PATH=/opt/git_repos/indrz-tu
-DEPLOY_FOLDER=/var/www/tu.indrz.com
+CODE_BASE_PATH=/opt/indrz
+DEPLOY_FOLDER=/var/www/example.com
 
 cd $CODE_BASE_PATH
 git pull
 
 
 cd $CODE_BASE_PATH/indrz
-source /opt/.venvs/indrz-tu/bin/activate
+source /opt/.venvs/indrz/bin/activate
 python manage.py collectstatic --clear <<<yes
 
 
@@ -16,7 +16,7 @@ sudo cp -r $DEPLOY_FOLDER/static/dist/. $DEPLOY_FOLDER/
 # clean up remove dist folder
 sudo rm -r $DEPLOY_FOLDER/static/dist
 
-sudo chown -R http-web:http-web /var/www/tu.indrz.com
+sudo chown -R http-web:http-web /var/www/example.com
 
 
 supervisorctl reload
