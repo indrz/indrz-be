e737a288086c9c5deaef069900ebf99dd64e4306
mdiener21 <mdiener21@gmail.com>
2021-08-29 21:27:34 +0200

    added enable admin panel templates

--- indrz/settings/settings.py
+++ indrz/settings/settings.py
@@ -96,6 +96,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
+                'django.template.context_processors.request',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
