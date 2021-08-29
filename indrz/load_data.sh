ded34209d69908c9a7b91061d7608c7e766d66fb
mdiener21 <mdiener21@gmail.com>
2021-08-29 22:46:02 +0200

    updated poi fixtures to load test data

--- indrz/load_data.sh
+++ indrz/load_data.sh
@@ -3,3 +3,4 @@ loaddata --app buildings fixture_buildings_campus.json
 loaddata --app buildings fixture_buildings_building.json
 loaddata --app poi_manager initial_poi_icons.json
 loaddata --app poi_manager initial_poi_categories.json
+loaddata --app poi_manager initial_poi.json
