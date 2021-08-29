dcf2b953a703d01e61d6a3ce19de15790f38a62b
mdiener21 <mdiener21@gmail.com>
2021-08-29 21:36:11 +0200

    updated buildings api viewsets

--- indrz/buildings/api.py
+++ indrz/buildings/api.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from buildings.models import BuildingFloor, Campus, Building
 from buildings.serializers import FloorSerializer, CampusSerializer, BuildingSerializer
 from rest_framework import viewsets
@@ -5,7 +8,7 @@ from rest_framework import viewsets
 
 class CampusViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    A simple ViewSet for viewing accounts.
+    Return a list of campuse
     """
     queryset = Campus.objects.all()
     serializer_class = CampusSerializer
@@ -13,7 +16,7 @@ class CampusViewSet(viewsets.ReadOnlyModelViewSet):
 
 class FloorViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    A simple ViewSet for viewing accounts.
+    Return a list of floors
     """
     queryset = BuildingFloor.objects.order_by('-floor_num').distinct('floor_num')
     serializer_class = FloorSerializer
@@ -21,7 +24,14 @@ class FloorViewSet(viewsets.ReadOnlyModelViewSet):
 
 class BuildingsViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    A simple ViewSet for viewing accounts.
+    Readonly return a list of buildings and floors per building
     """
     queryset = Building.objects.all()
     serializer_class = BuildingSerializer
+
+    @action(detail=True)
+    def floors(self,request, *args, **kwargs):
+        instance = self.get_object()
+        floor = BuildingFloor.objects.filter(fk_building_id=instance)
+        serializer = FloorSerializer(floor, many=True)
+        return Response(serializer.data)
