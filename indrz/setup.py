60a38b09e955c22bfc79fb269bc80d3e5c3fcd88
mdiener21 <mdiener21@gmail.com>
2022-11-22 08:04:33 +0100

    udpate to version and setup names

--- indrz/setup.py
+++ indrz/setup.py
@@ -2,9 +2,10 @@
 from setuptools import setup, find_packages
 
 setup(name='indrz',
-      version='3.0',
+      version='3.0.0',
       author='Michael Diener',
-      description='indoor maps and directions api',
+      description='indoor maps, wayfinding and directions api',
+      keywords=['indoor', 'gis', 'wayfinding', 'directions',],
       license='GNU 3',
       url='https://indrz.com',
       packages=find_packages(),
