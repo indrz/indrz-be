9ffed24e4755a2d7bda313adb434b8a71ac600b1
mdiener21 <mdiener21@gmail.com>
2023-01-17 18:33:55 +0100

    poi added poiImage model and admin viewer of poiimages

--- indrz/poi_manager/admin.py
+++ indrz/poi_manager/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 from django.utils.safestring import mark_safe
 from mptt.admin import DraggableMPTTAdmin
-from poi_manager.models import PoiCategory, Poi, PoiIcon
+from poi_manager.models import PoiCategory, Poi, PoiIcon, PoiImages
 
 class PoiAdmin(OSMGeoAdmin):
     list_display = ('id', 'name', 'name_de', 'name_en', 'category', 'image_preview', 'enabled')
     search_fields = ('name', 'category')
     list_filter = ('category', )
     readonly_fields = ('image_preview',)
-    default_lat = 5879660.89
-    default_lon = 1588005.35
+    default_lat = 6139956.30
+    default_lon = 1822274.03
     default_zoom = 16
 
     def image_preview(self, obj):
@@ -39,6 +39,20 @@ class PoiIconAdmin(admin.ModelAdmin):
 
     image_preview.short_description = 'Preview'
 
+class PoiImageAdmin(admin.ModelAdmin):
+    search_fields = ('poi.name', )
+    list_display = ('poi', 'thumbnails', 'images', 'image_preview')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.thumbnails:
+            return mark_safe('<img src="{0}" width="250" height="250" style="object-fit:contain" />'.format(obj.thumbnails.url))
+        else:
+            return '(No image)'
+
+    image_preview.short_description = 'Preview'
+
 admin.site.register(Poi, PoiAdmin)
 admin.site.register(PoiIcon, PoiIconAdmin)
 admin.site.register(PoiCategory, PoiCatAdmin)
+admin.site.register(PoiImages, PoiImageAdmin)
