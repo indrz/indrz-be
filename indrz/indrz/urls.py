f7636e1e0ea49f74b3f34750b836c5c5a8c32051
mdiener21 <mdiener21@gmail.com>
2024-12-01 16:28:28 +0100

    update indrz routers urls django 4.x

--- indrz/indrz/urls.py
+++ indrz/indrz/urls.py
@@ -1,17 +1,20 @@
-from api.search import search_any
-from django.conf.urls import url
+import os
+
+from django.conf import settings
+from django.urls import re_path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+from api.search import search_any
 from users.views import CustomAuthToken
-import os
 from .routers import router
 
 admin.site.site_header = 'INDRZ Manager'
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -30,18 +33,20 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('api/v1/api-auth/', include(('rest_framework.urls', 'rest_framework'), namespace='rest_framework')),
-    url(r'^api/v1/docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^api/v1/search/(?P<q>.+)', search_any, name='search'),
-    url(r'^api/v1/share/', include(('api.share_urls', 'share_result'), namespace='share_result')),
-    url(r'^api/v1/directions/', include(('routing.urls', 'directions'), namespace='directions')),
-    url(r'^api/v1/admin/', admin.site.urls),
-    url(r'^api/v1/poi/', include(('poi_manager.urls', 'poi'), namespace='poi')),
-    url(r'^api/v1/', include(router.urls))
+    re_path(r'^api/v1/docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^api/v1/search/(?P<q>.+)/$', search_any, name='search'),
+    re_path(r'^api/v1/share/', include(('api.share_urls', 'share_result'), namespace='share_result')),
+    re_path(r'^api/v1/directions/', include(('routing.urls', 'directions'), namespace='directions')),
+    re_path(r'^api/v1/admin/', admin.site.urls),
+    re_path(r'^api/v1/poi/', include(('poi_manager.urls', 'poi'), namespace='poi')),
+    re_path(r'^api/v1/', include(router.urls))
 ]
 
+
 urlpatterns += [
-    url(r'^api/v1/api-token-auth/', CustomAuthToken.as_view())
+    re_path(r'^api/v1/api-token-auth/', CustomAuthToken.as_view())
 ]
+
 
 urlpatterns += [
     # ... the rest of your URLconf goes here ...
@@ -50,29 +55,5 @@ urlpatterns += [
 
 if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += [
-        url(r'^translate/', include('rosetta.urls')),
+        re_path(r'^translate/', include('rosetta.urls')),
     ]
-
-
-##############################################
-# Static and media files in debug mode
-##############################################
-
-if settings.DEBUG:
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-    def mediafiles_urlpatterns(prefix):
-        """
-        Method for serve media files with runserver.
-        """
-        import re
-        from django.views.static import serve
-
-        return [
-            url(r'^%s(?P<path>.*)$' % re.escape(prefix.lstrip('/')), serve,
-                {'document_root': settings.MEDIA_ROOT})
-        ]
-
-    # Hardcoded only for development server
-    urlpatterns += staticfiles_urlpatterns(prefix="/static/")
-    urlpatterns += mediafiles_urlpatterns(prefix="/media/")
