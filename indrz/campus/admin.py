d6c16dbf9c40770aca7521b665ba214f6ef758c5
mdiener21 <mdiener21@gmail.com>
2025-03-08 19:20:16 +0100

    added campus new model

--- indrz/campus/admin.py
+++ indrz/campus/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Campus
 
 # Register your models here.
+@admin.register(Campus)
+class CampusAdmin(OSMGeoAdmin):
+    default_lon = 1587954  # Default longitude in map units
+    default_lat = 5879516  # Default latitude in map units
+    default_zoom = 15  # Default zoom level
+    pass
